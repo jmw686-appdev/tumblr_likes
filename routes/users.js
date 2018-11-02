850b5ae338341c1851e2bf3b526b1ea0aacfbf78
Jelani Woods <jmw686@nyu.edu>
2018-11-01 23:45:27 -0500

    add signup/signin pages

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/sign_up', function(req, res, next) {
+  res.render("users/sign_up")
+});
+
+router.get('/sign_in', function(req, res, next) {
+  res.render("users/sign_in")
+});
+
 module.exports = router;
